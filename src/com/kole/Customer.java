28b1fa5af63d7b687d13df1be1c21cd7ed4d50ed
Slobodan <slobodan.kosanovic@gmail.com>
2018-05-03 20:11:56 +0200

    a

--- src/com/kole/Customer.java
+++ src/com/kole/Customer.java
@@ -1,4 +1,13 @@
 package com.kole;
 
+import java.util.ArrayList;
+
 public class Customer {
+    private String name;
+    ArrayList<Double> transactions = new ArrayList<Double>();
+
+    public Customer(String name) {
+        this.name = name;
+        this.transactions = new ArrayList<Double>();
+    }
 }
