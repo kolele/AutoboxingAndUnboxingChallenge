e76ae31554b425014200c6e50b628a027289a544
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-05-16 16:54:24 +0200

    a

--- src/com/kole/Branch.java
+++ src/com/kole/Branch.java
@@ -1,5 +1,15 @@
 package com.kole;
 
+import java.util.ArrayList;
+
 public class Branch {
+    private String name;
+    ArrayList<Customer>customers;
+
+    public Branch(String name, ArrayList<Customer> customers, int initialTransaction) {
+        this.name = name;
+        this.customers = customers;
+    }
+
 
 }
--- src/com/kole/Customer.java
+++ src/com/kole/Customer.java
@@ -4,10 +4,29 @@ import java.util.ArrayList;
 
 public class Customer {
     private String name;
-    ArrayList<Double> transactions = new ArrayList<Double>();
+    private static ArrayList<Double> transactions;
+    private int initialTransaction;
 
-    public Customer(String name) {
+    public Customer(String name, ArrayList<Double> transactions, int initialTransaction) {
         this.name = name;
-        this.transactions = new ArrayList<Double>();
+        this.transactions = transactions;
+        this.initialTransaction = initialTransaction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+
+    public int getInitialTransaction() {
+        return initialTransaction;
+    }
+
+    public static void addInitialTransaction(double amount){
+        transactions.add(amount);
+        System.out.println("Added initial amount of " + amount);
     }
 }
