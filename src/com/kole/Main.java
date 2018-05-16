5067e2192f80db9ce4710a6aeb0e47c832533159
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-05-16 17:30:37 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -1,8 +1,10 @@
 package com.kole;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ArrayList<Integer> ara = new ArrayList<Integer>();
     }
 }
