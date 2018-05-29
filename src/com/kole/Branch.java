b8234a121008f59e1d3d882d65b7fbbcadc8168b
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-05-29 15:36:56 +0200

    a

--- src/com/kole/Branch.java
+++ src/com/kole/Branch.java
@@ -1,5 +1,28 @@
 package com.kole;
 
-public class Branch {
+import java.util.ArrayList;
 
+public class Branch {
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<>();
+    }
+
+    public boolean addNewCustomer(String name, int initialTransaction){
+        Customer tempCustomer = new Customer(name, initialTransaction);
+        for (int i=0; i<customers.size(); i++){
+            if (customers.get(i).getName().contains(name)){
+                System.out.println("Customer already exists");
+                return false;
+            } else customers.add(tempCustomer);
+
+        }return true;
+    }
+
+    public void addAddtionalTransactions(String name, int amount){
+        
+    }
 }
--- src/com/kole/Customer.java
+++ src/com/kole/Customer.java
@@ -1,5 +1,19 @@
 package com.kole;
 
+import java.util.ArrayList;
+
 public class Customer {
-    
+    private String name;
+    private int initialTransaction;
+    private ArrayList<Integer> transactions;
+
+    public Customer(String name, int initialTransaction) {
+        this.name = name;
+        this.initialTransaction = initialTransaction;
+        this.transactions = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
 }
